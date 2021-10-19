fda248336a75481bfeadde05aa107f425727c588
Alan W. Sun <asun17904@gmail.com>
2021-10-18 23:15:03 -0400

    created utilities for indexer: words, pagedir, and index data structure

---
+++ indexer/indexer.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "index.h"
+#include "word.h"
+#include "pagedir.h"
+
+
+/* Alan Sun
+ * CS50, Fall 2021, Tiny Search Engine: Indexer
+ * Indexer (indexer.c)
+ */
+
+void parse_args(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
+{
+  parse_args(argc, argv); 
+
+}
+
+void parse_args(int argc, char *argv[])
+{
+  if (argc != 4)
+    exit(1);
+  
+}
+
+
